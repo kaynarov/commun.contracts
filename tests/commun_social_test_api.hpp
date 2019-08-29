7aed695f93fd35a28b89a0db1d53d09e48e94ab5
boostmultifruit <boostmultifruit@mail.ru>
2019-08-29 09:22:31 +0300

    More tests on social contract #76

--- tests/commun_social_test_api.hpp
+++ tests/commun_social_test_api.hpp
@@ -7,6 +7,34 @@ struct commun_social_api: base_contract_api {
     commun_social_api(golos_tester* tester, name code)
         :   base_contract_api(tester, code) {}
 
+    action_result pin(account_name pinner, account_name pinning) {
+        return push(N(pin), pinner, args()
+            ("pinner", pinner)
+            ("pinning", pinning)
+        );
+    }
+
+    action_result unpin(account_name pinner, account_name pinning) {
+        return push(N(unpin), pinner, args()
+            ("pinner", pinner)
+            ("pinning", pinning)
+        );
+    }
+
+    action_result block(account_name blocker, account_name blocking) {
+        return push(N(block), blocker, args()
+            ("blocker", blocker)
+            ("blocking", blocking)
+        );
+    }
+
+    action_result unblock(account_name blocker, account_name blocking) {
+        return push(N(unblock), blocker, args()
+            ("blocker", blocker)
+            ("blocking", blocking)
+        );
+    }
+
     action_result update_meta(std::string avatar_url, std::string cover_url, 
                               std::string biography, std::string facebook, 
                               std::string telegram, std::string whatsapp, 
@@ -20,6 +48,12 @@ struct commun_social_api: base_contract_api {
                     ("whatsapp", whatsapp)
                     ("wechat", wechat)
                     );
+    }
+
+    action_result deletemeta(account_name account) {
+        return push(N(deletemeta), account, args()
+            ("account", account)
+        );
     }
 };
 
--- tests/commun_social_tests.cpp
+++ tests/commun_social_tests.cpp
@@ -13,18 +13,69 @@ class commun_social_tester : public golos_tester {
 protected:
     commun_social_api social;
 
+    std::vector<account_name> _users;
 public:
     commun_social_tester()
         : golos_tester(cfg::commun_social_name)
         , social({this, cfg::commun_social_name})
-    {
+        , _users{"dave"_n, "erin"_n} {
+        create_accounts(_users);
         create_account(cfg::commun_social_name);
         produce_block();
         install_contract(cfg::commun_social_name, contracts::commun_social_wasm(), contracts::commun_social_abi());
     }
+
+protected:
+
+    struct errors: contract_error_messages {
+        const string cannot_pin_yourself        = amsg("You cannot pin yourself");
+        const string cannot_unpin_yourself      = amsg("You cannot unpin yourself");
+        const string cannot_block_yourself      = amsg("You cannot block yourself");
+        const string cannot_unblock_yourself    = amsg("You cannot unblock yourself");
+    } err;
 };
 
 BOOST_AUTO_TEST_SUITE(commun_social_tests)
+
+BOOST_FIXTURE_TEST_CASE(pin_unpin_test, commun_social_tester) try {
+    BOOST_TEST_MESSAGE("pin unpin test");
+
+    BOOST_TEST_MESSAGE("--- pin: dave dave");
+    BOOST_CHECK_EQUAL(err.cannot_pin_yourself, social.pin("dave"_n, "dave"_n));
+    produce_block();
+
+    BOOST_TEST_MESSAGE("--- unpin: dave dave");
+    BOOST_CHECK_EQUAL(err.cannot_unpin_yourself, social.unpin("dave"_n, "dave"_n));
+    produce_block();
+
+    BOOST_TEST_MESSAGE("--- pin: dave erin");
+    BOOST_CHECK_EQUAL(success(), social.pin("dave"_n, "erin"_n));
+    produce_block();
+
+    BOOST_TEST_MESSAGE("--- unpin: dave erin");
+    BOOST_CHECK_EQUAL(success(), social.unpin("dave"_n, "erin"_n));
+    produce_block();
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(block_unblock_test, commun_social_tester) try {
+    BOOST_TEST_MESSAGE("block unblock test");
+
+    BOOST_TEST_MESSAGE("--- block: dave dave");
+    BOOST_CHECK_EQUAL(err.cannot_block_yourself, social.block("dave"_n, "dave"_n));
+    produce_block();
+
+    BOOST_TEST_MESSAGE("--- unblock: dave dave");
+    BOOST_CHECK_EQUAL(err.cannot_unblock_yourself, social.unblock("dave"_n, "dave"_n));
+    produce_block();
+
+    BOOST_TEST_MESSAGE("--- block: dave erin");
+    BOOST_CHECK_EQUAL(success(), social.block("dave"_n, "erin"_n));
+    produce_block();
+
+    BOOST_TEST_MESSAGE("--- unblock: dave erin");
+    BOOST_CHECK_EQUAL(success(), social.unblock("dave"_n, "erin"_n));
+    produce_block();
+} FC_LOG_AND_RETHROW()
 
 BOOST_FIXTURE_TEST_CASE(update_meta_test, commun_social_tester) try {
     BOOST_TEST_MESSAGE("update meta test");
@@ -36,4 +87,4 @@ BOOST_FIXTURE_TEST_CASE(update_meta_test, commun_social_tester) try {
                                                     "wechat"));
 } FC_LOG_AND_RETHROW()
 
-BOOST_AUTO_TEST_SUITE_END()
+BOOST_AUTO_TEST_SUITE_END() // commun_social_tests
