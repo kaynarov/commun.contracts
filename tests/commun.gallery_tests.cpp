fdd7a3026332347c9083337b8d10444bf90fe750
boostmultifruit <boostmultifruit@mail.ru>
2019-09-02 09:16:36 +0300

    Add more tests on cmmn.gallery #84

--- tests/commun.gallery_tests.cpp
+++ tests/commun.gallery_tests.cpp
@@ -38,15 +38,15 @@ public:
         install_contract(commun_emit_name, contracts::emit_wasm(), contracts::emit_abi());
         install_contract(cfg::token_name, contracts::token_wasm(), contracts::token_abi());
         install_contract(_code, contracts::gallery_wasm(), contracts::gallery_abi());
-        
+
         set_authority(commun_emit_name, cfg::mscsreward_perm_name, create_code_authority({_code}), "active");
         link_authority(commun_emit_name, commun_emit_name, cfg::mscsreward_perm_name, N(mscsreward));
-        
+
         std::vector<account_name> transfer_perm_accs{_code, commun_emit_name};
         std::sort(transfer_perm_accs.begin(), transfer_perm_accs.end());
         set_authority(commun_point_name, cfg::issue_permission, create_code_authority({commun_emit_name}), "active");
         set_authority(commun_point_name, cfg::transfer_permission, create_code_authority(transfer_perm_accs), "active");
-        
+
         link_authority(commun_point_name, commun_point_name, cfg::issue_permission, N(issue));
         link_authority(commun_point_name, commun_point_name, cfg::transfer_permission, N(transfer));
     }
@@ -63,7 +63,37 @@ public:
         const string overdrawn_balance = amsg("overdrawn balance");
         const string eval_period = amsg("evaluation period isn't over yet");
         const string insufficient_quantity(account_name account) { return amsg("insufficient quantity for " + string(account)); };
+        const string no_mosaic = amsg("mosaic doesn't exist");
+        const string no_points_provided = amsg("no points provided");
     } err;
+
+    void init() {
+        int64_t supply  = 5000000000000;
+        int64_t reserve = 50000000000;
+        uint16_t royalty = 2500;
+        int64_t min_gem_points = commun::safe_prop(commun::config::min_gem_cost, supply, reserve);
+        int64_t init_amount = supply / 2;
+
+        uint16_t annual_emission_rate = 1000;
+        uint16_t leaders_reward_prop = 1000;
+
+        BOOST_CHECK_EQUAL(success(), token.create(_commun, asset(reserve, token._symbol)));
+        BOOST_CHECK_EQUAL(success(), token.issue(_commun, _carol, asset(reserve, token._symbol), ""));
+
+        BOOST_CHECK_EQUAL(success(), point.create(_golos, asset(supply * 2, point._symbol), 10000, 1));
+        BOOST_CHECK_EQUAL(success(), point.set_freezer(commun::config::commun_gallery_name));
+
+        BOOST_CHECK_EQUAL(success(), push_action(commun_emit_name, N(create), commun_emit_name, mvo()
+            ("commun_symbol", point._symbol)("annual_emission_rate", annual_emission_rate)("leaders_reward_prop", leaders_reward_prop)));
+
+        BOOST_CHECK_EQUAL(success(), token.transfer(_carol, commun_point_name, asset(reserve, token._symbol), cfg::restock_prefix + point_code_str));
+        BOOST_CHECK_EQUAL(success(), point.issue(_golos, _golos, asset(supply, point._symbol), std::string(point_code_str) + " issue"));
+        BOOST_CHECK_EQUAL(success(), point.transfer(_golos, _alice, asset(init_amount, point._symbol)));
+        BOOST_CHECK_EQUAL(success(), point.transfer(_golos, _carol, asset(init_amount, point._symbol)));
+        BOOST_CHECK_EQUAL(success(), point.open(_code, point._symbol, _code));
+        BOOST_CHECK_EQUAL(success(), gallery.create(point._symbol));
+        BOOST_CHECK_EQUAL(success(), gallery.createmosaic(_alice, 1, asset(min_gem_points, point._symbol), royalty));
+    }
 };
 
 BOOST_AUTO_TEST_SUITE(gallery_tests)
@@ -75,19 +105,19 @@ BOOST_FIXTURE_TEST_CASE(basic_tests, commun_gallery_tester) try {
     uint16_t royalty = 2500;
     int64_t min_gem_points = commun::safe_prop(commun::config::min_gem_cost, supply, reserve);
     int64_t init_amount = supply / 2;
-    
+
     uint16_t annual_emission_rate = 1000;
     uint16_t leaders_reward_prop = 1000;
-    
+
     BOOST_CHECK_EQUAL(success(), token.create(_commun, asset(reserve, token._symbol)));
     BOOST_CHECK_EQUAL(success(), token.issue(_commun, _carol, asset(reserve, token._symbol), ""));
 
     BOOST_CHECK_EQUAL(success(), point.create(_golos, asset(supply * 2, point._symbol), 10000, 1));
     BOOST_CHECK_EQUAL(success(), point.set_freezer(commun::config::commun_gallery_name));
-    
+
     BOOST_CHECK_EQUAL(success(), push_action(commun_emit_name, N(create), commun_emit_name, mvo()
         ("commun_symbol", point._symbol)("annual_emission_rate", annual_emission_rate)("leaders_reward_prop", leaders_reward_prop)));
-    
+
     BOOST_CHECK_EQUAL(success(), token.transfer(_carol, commun_point_name, asset(reserve, token._symbol), cfg::restock_prefix + point_code_str));
     BOOST_CHECK_EQUAL(success(), point.issue(_golos, _golos, asset(supply, point._symbol), std::string(point_code_str) + " issue"));
     BOOST_CHECK_EQUAL(success(), point.transfer(_golos, _alice, asset(init_amount, point._symbol)));
@@ -103,7 +133,7 @@ BOOST_FIXTURE_TEST_CASE(basic_tests, commun_gallery_tester) try {
     produce_block();
     produce_block(fc::seconds(commun::config::default_evaluation_period - (cfg::block_interval_ms / 1000)));
     BOOST_CHECK_EQUAL(err.eval_period, gallery.claimgem(_alice, 1, point._symbol.to_symbol_code(), _alice));
-    
+
     produce_blocks(1);
 
     BOOST_CHECK_EQUAL(point.get_supply(), supply);
@@ -115,5 +145,75 @@ BOOST_FIXTURE_TEST_CASE(basic_tests, commun_gallery_tester) try {
 
 } FC_LOG_AND_RETHROW()
 
+
+BOOST_FIXTURE_TEST_CASE(create_tests, commun_gallery_tester) try {
+    BOOST_TEST_MESSAGE("create gallery tests");
+    int64_t supply  = 5000000000000;
+
+    BOOST_CHECK_EQUAL(err.no_balance, gallery.create(point._symbol));
+    BOOST_CHECK_EQUAL(success(), point.create(_golos, asset(supply * 2, point._symbol), 10000, 1));
+    BOOST_CHECK_EQUAL(success(), point.open(_code, point._symbol, _code));
+    BOOST_CHECK_EQUAL(success(), gallery.create(point._symbol));
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(createmosaic_tests, commun_gallery_tester) try {
+    BOOST_TEST_MESSAGE("createmosaic tests");
+    int64_t supply  = 5000000000000;
+    int64_t reserve = 50000000000;
+    uint16_t royalty = 2500;
+    int64_t min_gem_points = commun::safe_prop(commun::config::min_gem_cost, supply, reserve);
+    int64_t init_amount = supply / 2;
+
+    BOOST_CHECK_EQUAL(success(), point.create(_golos, asset(supply * 2, point._symbol), 10000, 1));
+
+    BOOST_CHECK_EQUAL(success(), token.create(_commun, asset(reserve, token._symbol)));
+    BOOST_CHECK_EQUAL(success(), token.issue(_commun, _carol, asset(reserve, token._symbol), ""));
+    BOOST_CHECK_EQUAL(success(), token.transfer(_carol, commun_point_name, asset(reserve, token._symbol), cfg::restock_prefix + point_code_str));
+
+    BOOST_CHECK_EQUAL(success(), point.open(_code, point._symbol, _code));
+    BOOST_CHECK_EQUAL(success(), point.issue(_golos, _golos, asset(supply, point._symbol), std::string(point_code_str) + " issue"));
+    BOOST_CHECK_EQUAL(success(), point.transfer(_golos, _alice, asset(init_amount, point._symbol)));
+    BOOST_CHECK_EQUAL(success(), gallery.create(point._symbol));
+
+    BOOST_CHECK_EQUAL(err.overdrawn_balance, gallery.createmosaic(_alice, 1, asset(point.get_amount(_alice) + 1, point._symbol), royalty));
+    BOOST_CHECK_EQUAL(err.insufficient_quantity(_alice), gallery.createmosaic(_alice, 1, asset(min_gem_points - 1, point._symbol), royalty));
+    BOOST_CHECK_EQUAL(success(), gallery.createmosaic(_alice, 1, asset(min_gem_points, point._symbol), royalty));
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(addtomosaic_tests, commun_gallery_tester) try {
+    BOOST_TEST_MESSAGE("addtomosaic tests");
+    BOOST_CHECK_EQUAL(err.no_mosaic, gallery.addtomosaic(_alice, 1, asset(point.get_amount(_carol), point._symbol), false, _carol));
+    init();
+
+    BOOST_CHECK_EQUAL(success(), gallery.addtomosaic(_alice, 1, asset(point.get_amount(_carol), point._symbol), false, _carol));
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(claimgem_tests, commun_gallery_tester) try {
+    BOOST_TEST_MESSAGE("claimgem tests");
+    int64_t supply  = 5000000000000;
+    init();
+
+    BOOST_CHECK_EQUAL(success(), gallery.addtomosaic(_alice, 1, asset(point.get_amount(_carol), point._symbol), false, _carol));
+    produce_block();
+    produce_block(fc::seconds(commun::config::default_evaluation_period - (cfg::block_interval_ms / 1000)));
+    BOOST_CHECK_EQUAL(err.eval_period, gallery.claimgem(_alice, 1, point._symbol.to_symbol_code(), _alice));
+
+    produce_blocks(1);
+
+    BOOST_CHECK_EQUAL(point.get_supply(), supply);
+    BOOST_CHECK_EQUAL(success(), gallery.claimgem(_alice, 1, point._symbol.to_symbol_code(), _carol)); //carol got nothing
+
+} FC_LOG_AND_RETHROW()
+
+BOOST_FIXTURE_TEST_CASE(provide_tests, commun_gallery_tester) try {
+    BOOST_TEST_MESSAGE("provide tests");
+
+    BOOST_CHECK_EQUAL(err.no_points_provided, gallery.provide(_alice, _carol, asset(1000, point._symbol)));
+    BOOST_CHECK_EQUAL(success(), gallery.provide(_alice, _carol, asset(0, point._symbol)));
+
+} FC_LOG_AND_RETHROW()
 
 BOOST_AUTO_TEST_SUITE_END()
