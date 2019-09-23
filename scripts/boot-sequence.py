96ae26e9f1b746a706a608d3319b4e5af7cc8c41
Semen Medvedev <smedvedev@golos.io>
2019-09-23 16:07:15 +0700

    Write base community test (post and vote) #136

--- scripts/boot-sequence.py
+++ scripts/boot-sequence.py
@@ -15,15 +15,12 @@ args = {
     'public_key':'GLS6MRyAjQq8ud7hVNYcfnVPJqcVpscN5So8BhtHuGYqET5GDW5CV',
     'private_key':'5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',
     'cyber_private_key':'5KQwrPbwdL6PhXujxW37FSSQZ1JiwsST4cqQzDeyXtP79zkvFD3',
-    'cyberway_contracts_dir': os.environ.get('CYBERWAY_CONTRACTS', default_contracts_dir),
     'commun_contracts_dir': os.environ.get('COMMUN_CONTRACTS', default_contracts_dir),
 }
 
 commun_boot_sequence=('{basedir}/scripts/commun-boot-sequence/commun-boot-sequence.py '
-                     '--cleos "{cleos}" --contracts-dir "{commun_contracts_dir}" ' 
-                     '--public-key {public_key} --private-key {private_key} '
-                     '--cyber-private-key {cyber_private_key} '
-                     '--docker --all').format(**args)
+                     '--contracts-dir "{commun_contracts_dir}" ' 
+                     '--private-key {private_key} ').format(**args)
 if subprocess.call(commun_boot_sequence, shell=True):
     print('commun-boot-sequence.py exited with error')
     sys.exit(1)
--- scripts/community-boot-sequence/community-boot-sequence.py
+++ scripts/community-boot-sequence/community-boot-sequence.py
@@ -7,70 +7,7 @@ import sys
 
 sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
 from testnet import *
-
-
-def createCommunity(community_name, owner_account, maximum_supply, reserve_amount, *, cw=10000, fee=100, annual_emission_rate=1000, leader_reward_prop=2000):
-    symbol = maximum_supply.symbol
-
-    # 1. Buy some value of COMMUN tokens
-    pushAction('cyber.token', 'issue', 'cmmn', {
-        'to':owner_account,
-        'quantity':reserve_amount,
-        'memo':"Reserve for {c}".format(c=community_name)
-    }, keys=args.commun_private_key)
-
-    # 2. Create community points (need cmmn permission)
-    pushAction('cmmn.point', 'create', 'cmmn.point', {
-        'issuer': owner_account,
-        'maximum_supply': maximum_supply,
-        'cw': cw,
-        'fee': fee
-    }, providebw='cmmn.point/cmmn', keys=args.commun_private_key)
-
-    # 3. Restock COMMUN tokens for community points
-    transfer(owner_account, 'cmmn.point', reserve_amount, 'restock: {code}'.format(code=symbol.code),
-        providebw='{acc}/cmmn'.format(acc=owner_account), keys=args.commun_private_key)
-    
-    # 4. Set ctrl-params (cmmn.ctrl:setparams)
-    pushAction('cmmn.ctrl', 'setparams', owner_account, {
-        "point":symbol.code,
-        "params":[
-            ["multisig_acc",{"name":owner_account}],
-            ["max_witnesses",{"max":21}],
-            ["multisig_perms", {"super_majority":0,"majority":0,"minority":0}],
-            ["max_witness_votes",{"max":30}]
-        ]
-    }, providebw='{acc}/cmmn'.format(acc=owner_account), keys=args.owner_private_key)
-
-    # 5. Create emission (cmmn.emit:create)
-    pushAction('cmmn.emit', 'create', 'cmmn.emit', {
-        "commun_symbol": symbol,
-        "annual_emission_rate": annual_emission_rate,
-        "leaders_reward_prop": leader_reward_prop
-    }, providebw='cmmn.emit/cmmn', keys=args.commun_private_key)
-
-    # 6. Open point balance for cmmn.gallery
-    pushAction('cmmn.point', 'open', 'cmmn', {
-        "owner": "cmmn.gallery",
-        "symbol": symbol,
-        "ram_payer": "cmmn"
-    }, keys=args.commun_private_key)
-
-    # 7. Set publication params (cmmn.gallery:setparams) (fix to check issuer permission instead of _self)
-    pushAction('cmmn.gallery', 'setparams', 'cmmn.gallery', {
-        "commun_code":symbol.code,
-        "params":[
-            ['st_max_comment_depth', {'value': 127}],
-            ['st_social_acc', {'value': 'cmmn.social'}],
-        ]
-    }, providebw='cmmn.gallery/cmmn', keys=args.commun_private_key)
-
-    # 8. Register community (cmmn.list:create)
-    pushAction('cmmn.list', 'create', 'cmmn.list', {
-        "token_name": symbol.code,
-        "community_name": community_name
-    }, providebw='cmmn.list/cmmn', keys=args.commun_private_key)
-
+import community
 
 # Command Line Arguments
 parser = argparse.ArgumentParser()
@@ -96,7 +33,7 @@ parser.add_argument('--leader-reward-prop', metavar='', help="Leader reward prop
 args = None
 args = parser.parse_args()
 
-# 0. Create community owner account
+# Create community owner account
 if not args.creator_account is None:
     createAccount(
         args.creator_account,
@@ -104,11 +41,13 @@ if not args.creator_account is None:
         args.owner_public_key,
         keys=args.creator_private_key)
 
-createCommunity(
+community.createCommunity(
     args.community_name,
     args.owner_account,
     Asset(args.maximum_supply),
     Asset(args.reserve_amount),
+    commun_private_key=args.commun_private_key,
+    owner_private_key=args.owner_private_key,
     cw=args.cw,
     fee=args.fee,
     annual_emission_rate=args.annual_emission_rate,
