0d739379403bd7c1419e0c6b43f47cc0b0d98601
boostmultifruit <boostmultifruit@mail.ru>
2019-10-02 10:41:53 +0300

    Add parameters structure into commun.list #196

--- commun.list/include/commun.list/commun.list.hpp
+++ commun.list/include/commun.list/commun.list.hpp
@@ -13,6 +13,11 @@ public:
     [[eosio::action]] void addinfo(symbol_code commun_code, std::string community_name, 
                                    std::string ticker, std::string avatar, std::string cover_img_link, 
                                    std::string description, std::string rules);
+
+    static inline auto get_community(name list_contract_account, symbol_code commun_code) {
+        tables::community community_tbl(list_contract_account, list_contract_account.value);
+        return community_tbl.get(commun_code.raw, "community not exists");
+    }
 };
 
 }
--- commun.list/include/commun.list/objects.hpp
+++ commun.list/include/commun.list/objects.hpp
@@ -9,6 +9,21 @@ struct community {
     symbol_code commun_code;
     std::string community_name;
 
+    uint16_t leaders_num = 21;
+
+    uint16_t emission_rate = 0; // TODO
+    uint16_t leaders_percent = 0; // TODO
+
+    uint16_t author_percent = 50 * cfg::_1percent;
+    int64_t collection_period = 7 * 24 * 60 * 60;
+    int64_t moderation_period = 10 * 24 * 60 * 60;
+    int64_t lock_period = 0; // TODO
+    uint16_t gems_per_day = 0; // TODO
+    uint16_t rewarded_mosaic_num = 20;
+    int64_t post_pledge_token = 0; // TODO
+    int64_t comment_pledge_token = 0; // TODO
+    int64_t min_gem_pledge_token = 0; // TODO
+
     uint64_t primary_key() const {
         return commun_code.raw();
     }
