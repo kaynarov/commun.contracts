75f4b4ed003eb14e3654a897d81459fc77216941
Andrew Falaleev <andrew.falaleev@gmail.com>
2019-10-17 19:55:38 +0700

    Add doxygen sections for commun.social. #304

---
+++ commun.social/include/commun.social/doxygen.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+/**
+ * \defgroup social comn.social
+ * \brief The commun.social smart contract contains a kit of the parameters.
+ */
+
+/**
+ * \defgroup social_class Contract description
+ * \ingroup social
+ */
