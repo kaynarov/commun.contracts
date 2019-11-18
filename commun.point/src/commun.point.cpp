936c65a9f83df3b8166ea66f1fb6e471bf3955b3
boostmultifruit <boostmultifruit@mail.ru>
2019-11-18 14:01:25 +0300

    Fix notifying issuer balance on points sell #454

--- commun.point/src/commun.point.cpp
+++ commun.point/src/commun.point.cpp
@@ -360,7 +360,7 @@ void point::do_transfer(name from, name to, const asset &quantity, const string 
 
         INLINE_ACTION_SENDER(eosio::token, transfer)(config::token_name, {_self, config::active_name},
             {_self, from, sub_reserve, quantity.symbol.code().to_string() + " sold"});
-        notify_balance_change(param.issuer, vague_asset(-quantity.amount));
+        notify_balance_change(param.issuer, vague_asset(-sub_reserve.amount));
         send_exchange_event(sub_reserve);
         if (fee_quantity.amount) {
             send_fee_event(fee_quantity);
