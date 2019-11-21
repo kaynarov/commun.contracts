ffacaf1278a49511c43e594f730be8b1b2b83c02
anazarov79 <a_n_nazarov@hotmail.com>
2019-11-21 18:54:41 +0700

    fix for doxygen text of social contract (commun.social.hpp, doxygen.hpp, types.h)

--- commun.social/include/commun.social/commun.social.hpp
+++ commun.social/include/commun.social/commun.social.hpp
@@ -5,7 +5,7 @@ namespace commun {
 using namespace eosio;
 
 /**
- * \brief This class implements comn.social contract behaviour
+ * \brief This class implements the \a commun.social contract behaviour.
  * \ingroup social_class
  */
 class commun_social: public contract {
@@ -13,89 +13,89 @@ public:
     using contract::contract;
 
     /**
-        \brief The pin action is used to add an account name to a pin-list.
+        \brief The \ref pin action is used to add an account name of interest to a pin-list.
 
-        \param pinner account name which is the pin-list owner and adds the name specified in the pinning field to the pin-list
-        \param pinning the account name that is being added to the pin-list
+        \param pinner account name who owns the pin-list and adds the \a pinning name to it
+        \param pinning account name to be added to the pin-list
 
-        Conditions for transaction execution:
-            - \a pinning account exists;
-            - \a pinner is not equal to \a pinning.
+        The action does not allow:
+            - to add nonexistent \a pinning account to pin-list;
+            - to add own name to pin-list. This means the \a pinner name must not match the \a pinning one.
 
-        Action do not stores any data in DB.
+        The action does not store any data in DB.
 
-        Performing the action requires the \a pinner account signature and client signature.
+        The action requires signing the \a pinner account and the trusted community client.
     */
     [[eosio::action]] void pin(name pinner, name pinning);
 
     /**
-        \brief The unpin action is used to remove an account name from a pin-list.
+        \brief The \ref unpin action is used to remove an account name from a pin-list.
 
-        \param pinner the account name that removes a name specified in the pinning field from the pin-list
-        \param pinning the account name that is about to be removed from the pin-list
+        \param pinner the account name who owns the pin-list and removes the \a pinning name from it
+        \param pinning the account name to be removed from the pin-list
 
-        Conditions for transaction execution:
-            - \a pinning account exists;
-            - \a pinner is not equal to \a pinning.
+        The action does not allow:
+            - to remove nonexistent \a pinning account from pin-list;
+            - to remove own name from pin-list. This means the \a pinner name must not match the \a pinning one.
 
-        Action do not stores any data in DB.
+        The action does not store any data in DB.
 
-        Performing the action requires the \a pinner account signature and client signature.
+        The action requires signing the \a pinner account and the trusted community client.
     */
     [[eosio::action]] void unpin(name pinner, name pinning);
 
     /**
-        \brief The block action is used to add an account name to "black" list.
+        \brief The \ref block action is used to add a name of unwanted account to «black» list.
 
-        \param blocker the account name which is the "black" list owner and adds the name specified in the blocking field to the "black" list
-        \param blocking the account name that is being added to the "black" list
+        \param blocker account name who owns the «black» list and adds the \a blocking name to it
+        \param blocking account name to be added to the «black» list
 
-        Conditions for transaction execution:
-            - \a blocker account exists;
-            - \a blocker is not equal to \a blocking.
+        The action does not allow:
+            - to add nonexistent \a blocking account to «black» list;
+            - to add own name to «black» list. This means the \a blocker name must not match the \a blocking one.
 
-        Action do not stores any data in DB.
+        The action does not store any data in DB.
 
-        Performing the action requires the \a blocker account signature and client signature.
+        The action requires signing the \a blocker account and the trusted community client.
     */
     [[eosio::action]] void block(name blocker, name blocking);
 
     /**
-        \brief The unblock action is used to remove an account name from "black" list.
+        \brief The \ref unblock action is used to remove an account name from «black» list.
 
-        \param blocker the account name which is the "black" list owner and removes the name specified in the blocking field from the "black" list
-        \param blocking the account name that is about to be removed from the "black" list
+        \param blocker account name who owns the «black» list and removes the \a blocking name from it
+        \param blocking account name to be removed from the «black» list
 
-        Conditions for transaction execution:
-            - \a blocker account exists;
-            - \a blocker is not equal to \a blocking.
+        The action does not allow:
+            - to remove nonexistent \a blocking account from «black» list;
+            - to remove own name from «black» list. This means the \a blocker name must not match the \a blocking one.
 
-        Action do not stores any data in DB.
+        The action does not store any data in DB.
 
-        Performing the action requires the \a blocker account signature and client signature.
+        The action requires signing the \a blocker account and the trusted community client.
     */
     [[eosio::action]] void unblock(name blocker, name blocking);
 
     /**
-        \brief The updatemeta action is used to fill, update, or delete account profile field values.
+        \brief The \ref updatemeta action is used to fill, update, or delete account profile field values.
 
-        \param account name of the account whose profile is being edited
-        \param meta a value of type of the \ref accountmeta structure
+        \param account the account whose profile is being edited
+        \param meta account profile (the accountmeta) to be edited
 
-        Action do not stores any data in DB.
+        The action does not store any data in DB.
 
-        Performing the action requires the \a account signature and client signature.
+        The action requires signing the \a account and the trusted community client.
     */
     [[eosio::action]] void updatemeta(name account, accountmeta meta);
 
     /**
-        \brief The deletemeta action is used for deleting an account profile.
+        \brief The \ref deletemeta action is used for deleting an account profile.
 
-        \param account is a name of the account whose profile is being deleted
+        \param account the account whose profile is being deleted
 
-        Action do not stores any data in DB.
+        The action does not store any data in DB.
 
-        Performing the action requires the \a account signature and client signature.
+        The action requires signing the \a account and the trusted community client.
     */
     [[eosio::action]] void deletemeta(name account);
 };
--- commun.social/include/commun.social/doxygen.hpp
+++ commun.social/include/commun.social/doxygen.hpp
@@ -1,8 +1,17 @@
 #pragma once
 
 /**
- * \defgroup social comn.social
- * \brief The commun.social smart contract allows the account add another accounts to pin-list, to "black" list, and update or delete its profile (meta).
+ * \defgroup social commun.social
+ * \brief The commun.social smart contract provides a user the following features:
+ - creating and editing user's profile (metadata);
+ - creating a pin-list to receive information about the publications of authors that the user is interested in;
+ - creating a so-called «black» list to block unwanted users.
+
+ <b>User profile</b> — user metadata is in the form of a structure. This  profile is created and edited by the user. The commun.social contract is not responsible for the storing of user metadata, but only controls whether the user has the right to change or delete metadata.
+
+ <b>Pin-list</b> — a user's list of account names that a given user is interested in. This list is created and edited by the user. It can be used in the client application to create subscriptions, including informing the given user (subscriber) about the appearance of a new post whose author’s name is contained in the pin list.
+ 
+ <b>«Black» list</b> —  a user's list of account names that this user characterizes as unwanted. This list is created and edited by the user. Using the «black» list allows the user to block comments and votes of accounts whose names are contained in this list.
  */
 
 /**
