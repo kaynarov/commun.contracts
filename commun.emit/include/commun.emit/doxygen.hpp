d9f015e319863353c1c7f5966a8b025acbf7d8f3
anazarov79 <a_n_nazarov@hotmail.com>
2019-12-16 20:45:49 +0700

    text for doxygen.hpp (list emit social)

--- commun.emit/include/commun.emit/doxygen.hpp
+++ commun.emit/include/commun.emit/doxygen.hpp
@@ -2,16 +2,18 @@
 
 /**
  * \defgroup emission commun.emit
- * \brief The smart contract implements the procedure for issuing new points to communities.
+ * \brief This smart contract implements the procedure for issuing new points to communities.
+ * \details The \a commun.emit contract produces points for each community at a specific frequency set for that community. It accepts requests from other contracts to issue and send points to them. The number of points issuing for a requestor, depends on such parameters as annual points emission, their current number, which has already been created and sent to this requestor, as well as on frequency of requests coming from the contract. Requests may come from \a commun.ctrl and \a commun.gallery contracts. The generated points for \a commun.ctrl contract are distributed as a reward to community leaders and users who voted for them. The generated points for \a commun.gallery contract are distributed among top mosaics.
  */
 
 /**
  * \defgroup emission_class Contract description
  * \ingroup emission
+ * \details The class contains actions that ensure an issuance of certain type points upon request from contracts.
  */
 
 /**
- * \details The structures represent the records of statistical table data in DB.
  * \defgroup emission_tables DB Stored structures
  * \ingroup emission
+ * \details The structures represent the records of statistical table data in DB.
  */
--- commun.list/include/commun.list/doxygen.hpp
+++ commun.list/include/commun.list/doxygen.hpp
@@ -2,16 +2,20 @@
 
 /**
  * \defgroup list commun.list
- * \brief The smart contract contains logic to configure communities.
+ * \brief This smart contract contains a logic to configure communities.
+ * \details The smart contract implements actions that allow a user to create a separate community, including specifying non-consensus information for the community. By default, community settings take hardcoded values. These settings can be changed by community leaders.
+
+ * Also, the actions allow a user to receive messages only from specific authors of interest to her/him. Also, it enables community leaders to ban publications from users whose content is inconsistent with community topics.
  */
 
 /**
  * \defgroup list_class Contract description
  * \ingroup list
+ * \details The class implements actions to configure the application and create a community in it.
  */
 
 /**
- * \details The commun.list contract creates the records when a community is formed. The records contain a community name, token name, allowable number of leaders, rules for the distribution of awards and other information that ensures the community functioning. These records are placed in structures and stored in DB.
  * \defgroup list_tables DB Stored structures
  * \ingroup list
+ * \details The commun.list contract creates the records when a community is formed. The records contain a community name, token name, allowable number of leaders, rules for the distribution of awards and other information that ensures the community functioning. These records are placed in structures and stored in DB.
  */
