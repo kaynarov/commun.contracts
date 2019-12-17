cca7320597068c44cd0bfea698435314a107f4eb
anazarov79 <a_n_nazarov@hotmail.com>
2019-12-17 17:42:49 +0700

    doxygen.hpp text (ctrl publication)

--- commun.ctrl/include/commun.ctrl/doxygen.hpp
+++ commun.ctrl/include/commun.ctrl/doxygen.hpp
@@ -2,26 +2,35 @@
 
 /**
  * \defgroup control commun.ctrl
- * \brief The commun.ctrl smart contract implements the logic of electing community leaders, including:
-   - logic of community leader registration;
-   - logic of voting for community leaders.
+ * \brief This smart contract implements the logic of electing and rewarding community leaders.
+ * \details <i>Community leader</i> — a user who is responsible for setting community parameters and moderating message content. Leaders are selected by community users through endless voting.
 
-   Besides of it, this contract contains actions providing creation and promotion of multi-signature transactions.
+ * A user creating the community automatically becomes its leader. However, it does not mean this user will always be on the leaderboard. Voting for leaders lasts all the time. Leaders are selected from among candidates — users who have registered themselves as candidates for leaders.
+
+ * A vote weight of each user depends on number of points reserved by her/him. It is assumed, the entire amount of reserved points reserved by user, equals to 100 % of vote. User can split the vote into parts and use them for several leaders. At any time user can withdraw back the vote from a leader and vote with it for another leader.
+
+ * Community leaders are candidates who received the maximum number of votes and included in the top list of leaders.
  */
 
 /**
  * \defgroup control_class Contract description
  * \ingroup control
+ * \details This class implements actions providing the following:
+ *  - community leader registration;
+ *  - voting for community leaders;
+ *  - rewarding community leaders.
+
+ * Besides of it, this class contains actions providing creation and promotion of multi-signature transactions.
  */
 
 /**
- * \details These structs implement the commun.ctrl contract data stored in DB.
  * \defgroup control_tables DB Stored structures
  * \ingroup control
+ * \details These structs implement the \a commun.ctrl contract data stored in DB.
  */
 
 /**
- * \details Events being sent to the Event-Engine by the commun.ctrl contract.
  * \defgroup control_events Events
  * \ingroup control
+ * \details Events being sent to the Event-Engine by the \a commun.ctrl contract.
  */
--- commun.publication/include/commun.publication/doxygen.hpp
+++ commun.publication/include/commun.publication/doxygen.hpp
@@ -1,17 +1,26 @@
 #pragma once
 
 /**
- * \defgroup publish comn.publish
- * \brief The commun.publish smart contract contains a kit of the parameters.
+ * \defgroup publish commun.publication
+ * \brief This smart contract implements actions ensuring the publication of messages, voting for them, as well as blocking them by community leaders in case of inappropriate content.
  */
 
 /**
  * \defgroup publish_class Contract description
  * \ingroup publish
+ * \details This class implements the actions that allow a user:
+ *  - to create and edit a message;
+ *  - to vote for other messages;
+ *  - to report about undesirable or suspicious messages to leaders.
+
+ * Also, the actions allow the community leaders:
+ *  - to lock a message whose contents is not relevant to community;
+ *  - to configure a gallery for community; 
+ *  - to modify tags of messages.
  */
 
 /**
- * \details This structs implements comn.publish contract data structures stored in a db
  * \defgroup publish_tables DB Stored structures
  * \ingroup publish
+ * \details These structures represent the tables data stored in DB.
  */
