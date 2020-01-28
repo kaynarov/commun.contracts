5a0a0a2673c286e6c3a65964addb52293f968359
Andrew Falaleev <andrew.falaleev@gmail.com>
2020-01-28 18:59:31 +0700

    Extend user profile with additional fields. #562

--- commun.social/include/commun.social/types.h
+++ commun.social/include/commun.social/types.h
@@ -26,15 +26,27 @@ struct accountmeta {
     optional<std::string> telegram; /**< User's Telegram account. */
     optional<std::string> whatsapp; /**< User's WhatsApp account. */
     optional<std::string> wechat; /**< User's WeChat account. */
+    optional<std::string> first_name; /**< User's First Name. */
+    optional<std::string> last_name; /**< User's Last Name. */
+    optional<std::string> country; /**< User's Country. */
+    optional<std::string> city; /**< User's City. */
+    optional<time_point_sec> birth_date; /**< User's Birth date. */
+    optional<std::string> instagram; /**< User's Instagram account. */
+    optional<std::string> linkedin; /**< User's LinkedIN account. */
+    optional<std::string> twitter; /**< User's Twitter account. */
+    optional<std::string> github; /**< User's Github account. */
+    optional<std::string> website_url; /**< User's WebSiteURL. */
 
 #ifndef UNIT_TEST_ENV
     EOSLIB_SERIALIZE(accountmeta,
-        (avatar_url)(cover_url)(biography)(facebook)(telegram)(whatsapp)(wechat))
+        (avatar_url)(cover_url)(biography)(facebook)(telegram)(whatsapp)(wechat)
+        (first_name)(last_name)(country)(city)(birth_date)(instagram)(linkedin)(twitter)(github)(website_url))
 #endif
 };
 
 #ifdef UNIT_TEST_ENV
 }} // eosio::testing
 FC_REFLECT(eosio::testing::accountmeta,
-    (avatar_url)(cover_url)(biography)(facebook)(telegram)(whatsapp)(wechat))
+    (avatar_url)(cover_url)(biography)(facebook)(telegram)(whatsapp)(wechat)
+    (first_name)(last_name)(country)(city)(birth_date)(instagram)(linkedin)(twitter)(github)(website_url))
 #endif
